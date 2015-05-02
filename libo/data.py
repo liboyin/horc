980b683344d1131a46e906743941e2b4573cb072
Libo Yin <andyinlibo@gmail.com>
2015-05-03 06:20:35 +1000

    migrated to probabilistic framework, added hue histogram classification

--- libo/data.py
+++ libo/data.py
@@ -13,27 +13,35 @@ img_dir = "data"
 num_sift_kp = 25
 
 
+def get_sift_descriptors(sift, img):
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    _, des = sift.detectAndCompute(img, None)
+    if len(des) > num_sift_kp:
+        des = des[:num_sift_kp]
+    return des.astype(np.uint32)
+
+
+def get_hue_histogram(img):
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+    hue_vec = img[:, :, 0].ravel()
+    return np.histogram(hue_vec, bins=181, range=(0, 180))[0]
+
+
 def make_df():
     df = pd.DataFrame(filter(lambda x: ".JPG" in x, listdir(img_dir)), columns=["name"])
     df["class"] = np.repeat(np.arange(50), 4)
     df["class"] = df["class"].astype("category")
-    key_points, kp_descriptors = list(), list()
+    sift_kp_des, hue_hist = list(), list()
     df_name = list(df["name"])
     sift = cv2.SIFT(nfeatures=num_sift_kp)  # @nfeatures is only approximate
     for i in range(0, 200):
-        progress = int(i / 200.0 * 1000) / 10.0
+        progress = int(i / 200.0 * 1000) / 10
         print("{} {}%".format(df_name[i], progress), end="\r")
         img = cv2.imread(join(img_dir, df_name[i]))
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        kp, des = sift.detectAndCompute(img, None)
-        if len(kp) > num_sift_kp:
-            kp = kp[:num_sift_kp]
-            des = des[:num_sift_kp]
-        key_points.append(map(lambda x: {"pt": x.pt, "angle": x.angle, "size": x.size,
-                                         "octave": x.octave, "response": x.response}, kp))
-        kp_descriptors.append(des.astype(np.uint16))
-    df["sift_key_points"] = key_points
-    df["sift_kp_descriptors"] = kp_descriptors
+        sift_kp_des.append(get_sift_descriptors(sift, img))
+        hue_hist.append(get_hue_histogram(img))
+    df["sift_kp_descriptors"] = sift_kp_des
+    df["hue_histogram"] = hue_hist
     return df
 
 
